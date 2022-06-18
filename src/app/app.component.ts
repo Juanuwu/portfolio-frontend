2287fd5d9c196b77200032cbb7af3dd04c458e53
Juan <juan.carosella@gmail.com>
2022-06-18 16:32:06 -0300

    test pero con los archivos bien ordenados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  @ViewChild('openForm') uwu: ElementRef;
+
+  constructor(private renderer2: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    console.log("uwu");
+  }
+
+  change(): void {
+
+    this.renderer2.setStyle(this.uwu.nativeElement, 'color', 'black');
+
+  }
+
 
 }
+
+
+
+
+
+
+
